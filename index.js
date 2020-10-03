8fda1c9ca5c2b7003fa88ed1e76fef2156934cbd
Shikukiza <57547930+Shikukiza@users.noreply.github.com>
2020-10-03 23:09:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
 const TelegramBot = require("node-telegram-bot-api");
-const token = "TelegramBot Token Kalian buat di @BotFather";
-const bot = new TelegramBot(token, { polling: true });
+const token = "ISI TOKEN @BotFather";
+const client = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  var bye = "bye";
-  if(msg.text.toString().toLowerCase().includes(bye)){
-    bot.sendMessage(msg.chat.id, "Selamat Pergi! " + msg.from.first_name);
-  }
-
-  var what = "idiot";
-  if(msg.text.includes(what)){
-    bot.kickChatMember(msg.chat.id, msg.from.id);
-  }
+client.on('message', (msg) => {
+  //replying
 });
 
-bot.onText(/\/start/, msg => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Selamat Datang, " +
-      msg.from.first_name +
-      "\nSilakan tekan commands atau button,\nyang berada dikeyboard anda!\n\nCommands:\n/dice - Kocok dadu secara random!\n/music [url] - Download music custom!\n/sendpic - Mengirim picture random!\n/nulis [teks] - Untuk nulis dibuku!\n/lirik [lagunya] - Untuk melihat lirik lagu!",
-    {
-      reply_markup: {
-        keyboard: [["/dice", "/music", "/sendpic", "/nulis", "/lirik"]]
-      }
-    }
-  );
+//akses untuk memulai bot!
+client.onText(/\/start/, msg => {
+  const chatId = msg.chat.id;
+  const reply = { reply_to_message_id: msg.message_id };
+  const name = msg.from.first_name;
+  client.sendMessage(chatId, "Welcome, " + msg.from.first_name + "!", reply);
 });
 
-bot.onText(/\/dice/, msg => {
-  bot.sendMessage(msg.chat.id, "1");
-});
-
-bot.onText(/\/rcode/, msg => {
-  bot.sendMessage(msg.chat.id, "Code:22345");
-});
-
-bot.onText(/\/sendpic/, msg => {
-  bot.sendPhoto(msg.chat.id, "https://images.app.goo.gl/1JF1zTiWz8HhS3kp8", {
-    caption: "Nama: Minecraft\nWeb: image.app.goo.gl\nBy: Reza"
-  });
+//akses untuk exit bot!
+client.onText(/\/exit/, msg => {
+  const chatId = msg.chat.id;
+  const reply = { reply_to_message_id: msg.message_id };
+  const name = msg.from.first_name;
+  client.sendMessage(chatId, "Bye-Bye, " + msg.from.first_name + "!", reply);
 });
